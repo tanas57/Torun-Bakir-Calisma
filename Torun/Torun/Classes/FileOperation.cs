282cccfc80d779eab29a4c02844c21cf7db7ed7f
M.Tayyip Muslu <muhammet.muslu@ceng.deu.edu.tr>
2019-07-09 09:08:59 +0300

    some classes added

---
+++ Torun/Torun/Classes/FileOperation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Torun.Classes
+{
+    public static class FileOperation
+    {
+        public static bool Write(string data, string filename)
+        {
+            TextWriter txt = new StreamWriter(filename);
+            txt.Write(data);
+            txt.Close();
+            return true;
+        }
+
+        public static string Read(string filename)
+        {
+            TextReader txt = new StreamReader(filename);
+            string data = txt.ReadLine();
+            if (data != String.Empty) return data;
+
+            return String.Empty;
+        }
+
+        public static bool FileExists(string filename)
+        {
+            return File.Exists(filename);
+        }
+    }
+}
