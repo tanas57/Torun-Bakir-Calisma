5e8113d898641e5901375928e0319d28d199135c
M.Tayyip Muslu <muhammet.muslu@ceng.deu.edu.tr>
2019-07-10 11:23:57 +0300

    language added

---
+++ Torun/Torun/Lang/ILanguage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torun.Lang
+{
+    public interface ILanguage
+    {
+        string WelcomeLoginFailedNotEnoughUserOrPassword { get; set; }
+        string WelcomeLoginFailedUserNotFind { get; set; }
+        string WelcomeLoginFailedWrongPassword { get; set; }
+        string WelcomeLoginSuccess { get; set; }
+
+    }
+}
---
+++ Torun/Torun/Lang/TR.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torun.Lang
+{
+    class TR : ILanguage
+    {
+        public string WelcomeLoginFailedNotEnoughUserOrPassword { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string WelcomeLoginFailedUserNotFind { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string WelcomeLoginFailedWrongPassword { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string WelcomeLoginSuccess { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
