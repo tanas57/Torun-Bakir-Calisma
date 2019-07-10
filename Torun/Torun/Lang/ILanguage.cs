368081e1abfee77f6bde704e0783dc5386f64b16
M.Tayyip Muslu <muhammet.muslu@ceng.deu.edu.tr>
2019-07-10 12:01:01 +0300

    welcome page and lang support

--- Torun/Torun/Lang/ILanguage.cs
+++ Torun/Torun/Lang/ILanguage.cs
@@ -8,10 +8,31 @@ namespace Torun.Lang
 {
     public interface ILanguage
     {
+        /* WELCOME PAGE STARTS*/
+
+        // LOGIN //
+        string WelcomeLoginTitle { get; set; }
+        string WelcomeLoginRemember { get; set; }
+        string WelcomeLoginButton { get; set; }
         string WelcomeLoginFailedNotEnoughUserOrPassword { get; set; }
         string WelcomeLoginFailedUserNotFind { get; set; }
         string WelcomeLoginFailedWrongPassword { get; set; }
         string WelcomeLoginSuccess { get; set; }
+        // REGISTER //
+        string WelcomeRegisterTitle { get; set; }
+        string WelcomeRegisterButton { get; set; }
+        string WelcomeSignSuccess { get; set; }
+        string WelcomeSignUserAlreadyExists { get; set; }
+        string WelcomeSignPasswordNotEnough { get; set; }
+        string WelcomeSignPasswordsNotMatch { get; set; }
+        string WelcomeSignUsernameLenghtMustBeGreaterThanThree { get; set; }
+        string WelcomeSignFillAllFields { get; set; }
+        string WelcomeRegisterTitleUsername { get; set; }
+        string WelcomeRegisterTitlePassword { get; set; }
+        string WelcomeRegisterTitlePasswordAgain { get; set; }
+        string WelcomeRegisterTitleFirstName { get; set; }
+        string WelcomeRegisterTitleLastName { get; set; }
 
+        /* WELCOME PAGE ENDS*/
     }
 }
--- Torun/Torun/Lang/TR.cs
+++ Torun/Torun/Lang/TR.cs
@@ -8,9 +8,34 @@ namespace Torun.Lang
 {
     class TR : ILanguage
     {
-        public string WelcomeLoginFailedNotEnoughUserOrPassword { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string WelcomeLoginFailedUserNotFind { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string WelcomeLoginFailedWrongPassword { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string WelcomeLoginSuccess { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        /* 
+         * WELCOME PAGE
+         */
+
+        /* LOGIN */
+        public string WelcomeLoginTitle { get => "Giriş"; set => throw new NotImplementedException(); }
+        
+        public string WelcomeLoginFailedNotEnoughUserOrPassword { get => "Kullanıcı adı veya şifre yanlış"; set => throw new NotImplementedException(); }
+        public string WelcomeLoginFailedUserNotFind { get => "Kullanıcı bulunamadı"; set => throw new NotImplementedException(); }
+        public string WelcomeLoginFailedWrongPassword { get => "Şifre yanlış"; set => throw new NotImplementedException(); }
+        public string WelcomeLoginSuccess { get => "Giriş başarılı"; set => throw new NotImplementedException(); }
+        public string WelcomeLoginRemember { get => "Girişi hatırla"; set => throw new NotImplementedException(); }
+        public string WelcomeLoginButton { get => "Giriş yap"; set => throw new NotImplementedException(); }
+        public string WelcomeRegisterTitle { get => "Yeni üyelik"; set => throw new NotImplementedException(); }
+        public string WelcomeSignSuccess { get => "Kayıt başarılı, giriş yapabilirsiniz"; set => throw new NotImplementedException(); }
+        public string WelcomeSignUserAlreadyExists { get => "Kullanıcı adı sistemde mevcut"; set => throw new NotImplementedException(); }
+        public string WelcomeSignPasswordNotEnough { get => "Şifre yetersiz"; set => throw new NotImplementedException(); }
+        public string WelcomeSignPasswordsNotMatch { get => "Şifreler eşleşmiyor"; set => throw new NotImplementedException(); }
+        public string WelcomeSignUsernameLenghtMustBeGreaterThanThree { get => "Kullanıcı adı en az 3 karakter olmalı"; set => throw new NotImplementedException(); }
+        public string WelcomeSignFillAllFields { get => "Boş alanları doldurun"; set => throw new NotImplementedException(); }
+        public string WelcomeRegisterButton { get => "Üye ol"; set => throw new NotImplementedException(); }
+       
+        public string WelcomeRegisterTitleUsername { get => "kullanıcı adı"; set => throw new NotImplementedException(); }
+        public string WelcomeRegisterTitlePassword { get => "şifre"; set => throw new NotImplementedException(); }
+        public string WelcomeRegisterTitlePasswordAgain { get => "şifre tekrarı"; set => throw new NotImplementedException(); }
+        public string WelcomeRegisterTitleFirstName { get => "kullanıcı ismi"; set => throw new NotImplementedException(); }
+        public string WelcomeRegisterTitleLastName { get => "kullanıcı soyismi"; set => throw new NotImplementedException(); }
+        
+        /* REGISTER */
     }
 }
