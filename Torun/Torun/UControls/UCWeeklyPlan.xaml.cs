69fbeb1f1b366fa6516134adb7fe106294ccef2e
M.Tayyip Muslu <muhammet.muslu@ceng.deu.edu.tr>
2019-07-19 12:00:26 +0300

    date interval added to weekly plan

--- Torun/Torun/UControls/UCWeeklyPlan.xaml.cs
+++ Torun/Torun/UControls/UCWeeklyPlan.xaml.cs
@@ -21,7 +21,7 @@ namespace Torun.UControls
             InitializeComponent();
             db = mainWindow.db;
             currentUser = mainWindow.currentUser;
-            Grid_todoList0.ItemsSource = db.ListWeeklyPlanDay(currentUser, new DateTime(2019,07,22));
+            Grid_todoList0.ItemsSource = db.ListWeeklyPlanDay(currentUser, new DateTime(2019,07,22)); txt_MondayCount.Text = Grid_todoList0.Items.Count.ToString() + " adet kayıt";
             Grid_todoList1.ItemsSource = db.ListWeeklyPlanDay(currentUser, new DateTime(2019, 07, 23));
             Grid_todoList2.ItemsSource = db.ListWeeklyPlanDay(currentUser, new DateTime(2019, 07, 24));
             Grid_todoList3.ItemsSource = db.ListWeeklyPlanDay(currentUser, new DateTime(2019, 07, 25));
@@ -36,6 +36,8 @@ namespace Torun.UControls
             lbl_dayThursday.Content = mainWindow.language.UCWeeklyPlanDaysThursday;
             lbl_dayFriday.Content = mainWindow.language.UCWeeklyPlanDaysFriday;
             lbl_currentTime.Content = mainWindow.language.UCWeeklyPlanCurrentTime;
+
+            lbl_dates.Text = DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek + (int)DayOfWeek.Monday).ToShortDateString() + " - " + DateTime.Today.AddDays(-(int)DateTime.Today.DayOfWeek + (int)DayOfWeek.Friday).ToShortDateString();
         }
     }
 }
