4499b3b8f49273272aaf2dea18edf25ffc7f2567
M.Tayyip Muslu <muhammet.muslu@ceng.deu.edu.tr>
2019-07-29 07:57:11 +0300

    db changes

--- Torun/Database/databaseSQL.sql
+++ Torun/Database/databaseSQL.sql
@@ -1,7 +1,7 @@
 --CREATE DATABASE PLAN_TRACERDB;
 
 -- users table
-CREATE TABLE users(
+CREATE TABLE Users(
 	id INT PRIMARY KEY IDENTITY(1,1),
 	firstname VARCHAR(50) NOT NULL,
 	lastname VARCHAR(50) NOT NULL,
@@ -11,35 +11,35 @@ CREATE TABLE users(
 	last_login DATETIME NOT NULL DEFAULT NULL,
 	login_status TINYINT NOT NULL DEFAULT 0, -- 1: on, 2 off 
 	user_status TINYINT NOT NULL DEFAULT 1,  -- 1: active, 2: disactive 3: banned
-	register_data DATETIME NOT NULL
+	register_date DATETIME NOT NULL
 );
 -- to do list table
-CREATE TABLE todoList(
+CREATE TABLE TodoList(
 	id INT PRIMARY KEY IDENTITY(1,1),
-	request_number VARCHAR(100) DEFAULT NULL,
+	request_number VARCHAR(100) NOT NULL,
 	priority TINYINT NOT NULL DEFAULT 2, -- 1: high, 2: normal, 3: low
 	description TEXT,
 	user_id INT NOT NULL, -- which user is
 	add_time DATETIME NOT NULL,
-	status TINYINT NOT NULL DEFAULT 0 -- 1: added, 2: updated, 3: deleted
+	status TINYINT NOT NULL DEFAULT 1 -- 1: added, 2: updated, 3: deleted
 );
 
-alter table todoList
-add constraint FK_todoList_user 
-foreign key  (user_id) references users (id);
+alter table TodoList
+add constraint FK_TodoList_Users 
+foreign key  (user_id) references Users (id);
 
 -- plan table
 
-CREATE TABLE plans(
+CREATE TABLE Plans(
 	id INT PRIMARY KEY IDENTITY(1,1),
 	work_id INT NOT NULL,
 	add_time DATETIME NOT NULL,
 	work_plan_time DATETIME NOT NULL
 );
 
-alter table plans
-add constraint FK_plan_todoList 
-foreign key  (work_id) references todoList (id);
+alter table Plans
+add constraint FK_plan_TodoList 
+foreign key  (work_id) references TodoList (id);
 
 -- work done table
 
@@ -53,14 +53,13 @@ CREATE TABLE WorkDone(
 );
 
 alter table WorkDone
-add constraint FK_WorkDone_todoList 
-foreign key  (plan_id) references plans(id);
+add constraint FK_WorkDone_TodoList 
+foreign key  (plan_id) references Plans(id);
 
-insert into users (firstname, lastname, user_name, pc_name, last_login, login_status, user_status) values ('M.Tayyip', 'Muslu', 'tanas57', 'MusluNET', '2019-07-08', 0, 1,'202cb962ac59075b964b07152d234b70', '2019-07-08');
+insert into Users (firstname, lastname, user_name, password, pc_name, last_login, login_status, user_status, register_date) values ('M.Tayyip', 'Muslu', 'tanas57', '68053af2923e00204c3ca7c6a3150cf7', 'MusluNET', '2019-07-08', 0, 1,'2019-07-08');
 
-insert into todoList(request_number, priority, user_id, description, add_time, status)
-values('2900', 2, 1, 'Talep açıklaması gelecek', '2019-07-07', 1);
-insert into todoList(request_number, priority, user_id, description, add_time, status)
-values(NULL, 3, 1, 'Talep açıklaması gelecek', '2019-07-07', 1);
-insert into todoList(request_number, priority, user_id, description, add_time, status)
-values('2900', 1, 1, 'Talep açıklaması gelecek', '2019-07-07', 1);
+insert into TodoList(request_number, priority, description, user_id, add_time, status)
+values('2900', 2, 'Talep açıklaması gelecek', 1, '2019-07-07', 1);
+
+insert into TodoList(request_number, priority, description, user_id, add_time, status)
+values('2905', 2, 'Talep açıklaması gelecek', 1, '2019-07-29', 1);
